8025c5c29fc4e643d8824ed33d62a917a402976c
grace-malae <grace.t.malae@gmail.com>
2021-04-21 20:37:02 +1200

    fixed background colour

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -19,7 +19,7 @@ function Main () {
           <div className="mb-4">
             <div className="flex flex-wrap mb-8 mt-8">
               <label className="block text-blue-300 py-2 font-bold mb-2 md:w-1/5 " htmlFor="firstName">First Name</label>
-              <input className="shadow appearance-none border rounded w-full md:w-4/5 p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out" id="firstName" type="text" placeholder="kahu" />
+              <input className="shadow appearance-none border rounded w-full md:w-4/5 p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out" id="firstName" type="text" placeholder="Kahu" />
             </div>
             <div className="flex flex-wrap mb-8 mt-8">
               <label className="block text-blue-300 py-2 font-bold mb-2 md:w-1/5" htmlFor="emailAddress">Email</label>
@@ -27,7 +27,17 @@ function Main () {
             </div>
             <div className="flex flex-wrap mb-8 mt-8">
               <label className="block text-blue-300 py-2 font-bold mb-2 md:w-1/5" htmlFor="birthday">Birthday</label>
-              <input className="shadow appearance-none border rounded w-full md:w-4/5 p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out" id="birthday" type="text" placeholder="17/07/2002" />
+              <input className="shadow appearance-none border rounded w-full md:w-4/5 p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out" id="birthday" type="date" />
+            </div>
+            <div className="flex flex-wrap mb-8 mt-8">
+              <label className="block text-blue-300 py-2 font-bold mb-2 md:w-1/5" htmlFor="where">I am...</label>
+              <select className="shadow appearance-none border rounded w-full md:w-4/5 p-3 text-gray-700 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out" name="where" id="where">
+                <option placeholder="please select">please select</option>
+                <option value="Māori">Māori</option>
+                <option value="Living overseas but was born in New Zealand">Living overseas but was born in New Zealand</option>
+                <option value="Living in New Zealand but was born overseas">Living in New Zealand but was born overseas</option>
+
+              </select>
             </div>
           </div>
           <div className="flex items-center justify-between pt-4">
