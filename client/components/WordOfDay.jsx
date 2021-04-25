2c3a0de70606e6b5a442383dabbc4c129c6c59f2
grace-malae <grace.t.malae@gmail.com>
2021-04-25 16:56:53 +1200

    change capitalisation

--- client/components/WordOfDay.jsx
+++ client/components/WordOfDay.jsx
@@ -6,8 +6,8 @@ function WordOfDay () {
 
       <div className="flex flex-col w-full p-12 mb-12 overflow-auto justify-center lg:items-start">
         <p className="font-bold text-white opacity-75 text-xl md:text-3xl mb-8 text-center md:text-left">Today&apos;s word is...</p>
-        <span className="font-bold text-5xl md:text-7xl lg:text-9xl self-center bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-pink-500 hover:from-pink-500 hover:to-blue-500 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out">AROHA</span>
-        <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold self-center text-center md:text-left">(ah-roh-ha)</h1>
+        <span className="font-bold text-5xl md:text-7xl lg:text-9xl self-center bg-clip-text text-transparent bg-gradient-to-r from-green-500 to-pink-500 hover:from-pink-500 hover:to-blue-500 leading-tight focus:ring transform transition hover:scale-105 duration-300 ease-in-out text-shadow-2xl">AROHA</span>
+        <h1 className="my-4 text-3xl md:text-5xl text-white opacity-75 font-bold self-center text-center md:text-left">(uh-raw-huh)</h1>
         <h1 className="my-4 text-3xl md:text-5xl font-bold self-center bg-clip-text text-transparent bg-gradient-to-r from-green-400 via-pink-500 to-purple-500">love</h1>
       </div>
     </div>
